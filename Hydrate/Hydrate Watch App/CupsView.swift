9cb9a5324854d244f8dc4cadd3f1afe1b9c101e4
Maryam16mgh <54358251+Maryam16mgh@users.noreply.github.com>
2024-02-13 11:35:20 +0300

    Add files via upload

---
+++ Hydrate/Hydrate Watch App/CupsView.swift
@@ -0,0 +1,18 @@
+//
+//  CupsView.swift
+//  Hydrate Watch App
+//
+//  Created by Maryam Mohammad on 03/08/1445 AH.
+//
+
+import SwiftUI
+
+struct CupsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CupsView()
+}
---
+++ Hydrate/Hydrate Watch App/LiterView.swift
@@ -0,0 +1,18 @@
+//
+//  LiterView.swift
+//  Hydrate Watch App
+//
+//  Created by Maryam Mohammad on 03/08/1445 AH.
+//
+
+import SwiftUI
+
+struct LiterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LiterView()
+}
