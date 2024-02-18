0d73379dc50fc65a17017332f9d3e9d5d7ad6d37
maryam16mgh <mmgh16@gmail.com>
2024-02-18 12:45:34 +0300

    LiterView

---
+++ Hydrate/Hydrate/PageControl.swift
@@ -0,0 +1,18 @@
+//
+//  PageControl.swift
+//  Hydrate
+//
+//  Created by Maryam Mohammad on 08/08/1445 AH.
+//
+
+import SwiftUI
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageControl()
+}
