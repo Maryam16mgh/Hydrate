5aaa8459da805770d05b46502d2de1df42431432
maryam16mgh <mmgh16@gmail.com>
2024-02-20 09:49:32 +0300

    watch

---
+++ Hydrate/Hydrate Watch App/PageControl.swift
@@ -0,0 +1,18 @@
+//
+//  PageControl.swift
+//  Hydrate Watch App
+//
+//  Created by Maryam Mohammad on 09/08/1445 AH.
+//
+
+import SwiftUI
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageControl()
+}
